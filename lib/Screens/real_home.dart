90ef1ff54113198586b311a79dc38e10ab8b7c38
Shubhveer Singh Chaudhary <shubhchaudhary1203@gmail.com>
2021-11-22 20:41:51 +0530

    gradient

--- lib/Screens/real_home.dart
+++ lib/Screens/real_home.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
 
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
@@ -140,7 +142,7 @@ class _carouselState extends State<carousel> {
                   );
                 },
                 options: CarouselOptions(
-                    autoPlay: true,
+                    autoPlay: false,
                     autoPlayAnimationDuration: Duration(milliseconds: 500),
                     enlargeCenterPage: true),
               ),
@@ -151,18 +153,68 @@ class _carouselState extends State<carousel> {
 }
 
 Widget MovieCard(Movie movie, int index) => Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color: Colors.grey[600],
-      ),
       margin: EdgeInsets.symmetric(horizontal: 0),
-      child: Stack(children: [
-        Positioned(bottom: 10, left: 10, child: Text(movie.title)),
-        ClipRRect(
-          borderRadius: BorderRadius.circular(30),
-          child: CachedNetworkImage(
-            imageUrl: "https://image.tmdb.org/t/p/original" + movie.backdropURL,
+      child: Stack(clipBehavior: Clip.none, children: [
+        Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: CachedNetworkImageProvider(
+                    "https://image.tmdb.org/t/p/original" + movie.backdropURL,
+                  ),
+                )),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                gradient: LinearGradient(
+                    begin: FractionalOffset.topCenter,
+                    end: FractionalOffset.bottomCenter,
+                    colors: [Colors.grey.withOpacity(0.0), Colors.black],
+                    stops: [0.0, 1.0]),
+              ),
+            )),
+        Positioned(
+          bottom: -10,
+          left: 10,
+          child: Row(
+            children: [
+              Container(
+                width: 100,
+                child: CachedNetworkImage(
+                    imageUrl:
+                        "https://image.tmdb.org/t/p/original" + movie.posterURL,
+                    fit: BoxFit.cover),
+              ),
+              SizedBox(width: 10),
+              Column(
+                children: [
+                  ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minWidth: 10,
+                      maxWidth: 200,
+                    ),
+                    child: AutoSizeText(
+                      movie.title,
+                      maxLines: 2,
+                      style: TextStyle(fontSize: 18),
+                      maxFontSize: 32,
+                    ),
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.star,
+                        color: Colors.yellowAccent,
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ],
           ),
-        ),
+        )
       ]),
     );
